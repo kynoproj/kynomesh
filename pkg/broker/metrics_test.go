06a99eae9dc5f0cb045c07c36c3cc6bec37daf38
Derek Wang <whynowy@gmail.com>
2026-06-22 08:41:02 -0700

    .
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/broker/metrics_test.go
+++ pkg/broker/metrics_test.go
@@ -23,8 +23,8 @@ import (
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
-	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/client_golang/prometheus/testutil"
+	dto "github.com/prometheus/client_model/go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
