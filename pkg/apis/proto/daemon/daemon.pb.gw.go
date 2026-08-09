1606f9a4990cc755377022468c4bbbdb6003fc17
Derek Wang <whynowy@gmail.com>
2026-08-09 16:54:17 -0700

    .
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/apis/proto/daemon/daemon.pb.gw.go
+++ pkg/apis/proto/daemon/daemon.pb.gw.go
@@ -43,9 +43,6 @@ func request_DaemonService_GetAgentDeployMetrics_0(ctx context.Context, marshale
 		metadata runtime.ServerMetadata
 		err      error
 	)
-	if req.Body != nil {
-		_, _ = io.Copy(io.Discard, req.Body)
-	}
 	val, ok := pathParams["name"]
 	if !ok {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
@@ -59,6 +56,9 @@ func request_DaemonService_GetAgentDeployMetrics_0(ctx context.Context, marshale
 	}
 	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DaemonService_GetAgentDeployMetrics_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
 	}
 	msg, err := client.GetAgentDeployMetrics(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
